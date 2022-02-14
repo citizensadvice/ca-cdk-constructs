2df99562fd23a31a016ddc6742aecbec46116ee3
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2022-02-14 13:42:17 +0000

    Specificy requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(name='cdk-constructs',
       author='Citizens Advice',
       author_email='ca-devops@citizensadvice.org.uk',
       license='MIT',
+      install_requires=[
+          'aws-cdk-lib>=2.4.0,<3.0.0',
+          'constructs>=10.0.0,<11.0.0',
+          'cdk-remote-stack>=2.0.8,<3.0.0'
+      ],
       packages=['cdk_constructs'],
       package_data={'cdk_constructs': ['assets/*.json']},
       include_package_data=True)
