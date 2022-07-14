617898100f664b0f8a37e59a5dbe8c5d5d663f41
Zhelyan <zhelyan.panchev@citizensadvice.org.uk>
2022-07-14 17:49:11 +0100

    style

--- tests/unit/test_domain_configuration.py
+++ tests/unit/test_domain_configuration.py
@@ -3,7 +3,9 @@ from ca_cdk_constructs import DomainConfiguration
 
 def test_non_prod_domain_properties():
 
-    domain_props = DomainConfiguration(record_name="foo", env_name="qa", apex_domain="somedomain.org.uk")
+    domain_props = DomainConfiguration(
+        record_name="foo", env_name="qa", apex_domain="somedomain.org.uk"
+    )
 
     assert domain_props.domain == "foo.qa.somedomain.org.uk"
     assert domain_props.ingress_domain == "foo-ingress.qa.somedomain.org.uk"
@@ -12,7 +14,9 @@ def test_non_prod_domain_properties():
 
 def test_prod_domain_properties():
 
-    domain_props = DomainConfiguration(record_name="foo", env_name="prod", apex_domain="somedomain.org.uk")
+    domain_props = DomainConfiguration(
+        record_name="foo", env_name="prod", apex_domain="somedomain.org.uk"
+    )
 
     assert domain_props.domain == "foo.somedomain.org.uk"
     assert domain_props.ingress_domain == "ingress.foo.somedomain.org.uk"
