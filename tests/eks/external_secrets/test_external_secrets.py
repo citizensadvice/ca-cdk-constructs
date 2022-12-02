2ab3255d08ed32e6bb7a4dec983845eca4f41740
Zhelyan <zhelyan.panchev@citizensadvice.org.uk>
2022-12-02 17:33:26 +0000

    external secrets: more tests

--- tests/eks/external_secrets/test_external_secrets.py
+++ tests/eks/external_secrets/test_external_secrets.py
@@ -3,7 +3,7 @@ from cdk8s import App, Chart
 from ca_cdk_constructs.eks.external_secrets import (
     ExternalSecrets,
     ExternalSecretSource,
-    ExternalSecretStore,
+    ExternalSecretStore, ExternalSecret,
 )
 
 
@@ -38,6 +38,33 @@ def test_external_secrets(snapshot):
             external_secret_name="app-db-secret",
         ),
     )
+    assert app.synth_yaml() == snapshot
+
+
+def test_external_secrets_direct_usage(snapshot):
+    ssm_secret_name = "ssm-secret"
+
+    app = App()
+    chart = Chart(app, "ExternSecretsChart")
+
+    ExternalSecret(chart, "secret1",
+                   secret_store=ExternalSecretStore.VAULT,
+                   k8s_secret_name="app-vault-secret",
+                   source_secret="path/to/secret",
+                   secret_mappings={"key": "ENV_VAR", "key.two": ""},
+                   metadata={"name": "app-vault-secret"},
+                   )
+
+    source2 = ExternalSecretSource(
+        store=ExternalSecretStore.AWS_SSM,
+        k8s_secret_name="app-db-secret",
+        source_secret=ssm_secret_name,
+        secret_mappings={"username": "DB_USER"},
+        external_secret_name="app-db-secret",
+    )
+
+    ExternalSecret.from_external_secret_source(chart, "secret2", source2)
+
     assert app.synth_yaml() == snapshot
 
 
