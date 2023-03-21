ccfeecd85df4a2ffd91615d8bd02e1405b48f7fc
Zhelyan Panchev <zhelyan.panchev@citizensadvice.org.uk>
2023-03-21 15:01:34 +0000

    feat(external secrets): AWS secrets cdk8s chart

--- ca_cdk_constructs/eks/external_secrets/__init__.py
+++ ca_cdk_constructs/eks/external_secrets/__init__.py
@@ -1,1 +1,3 @@
-from .external_secret import *
+from .external_secrets_aws_secret_store import ExternalSecretsAwsSecretStore
+from .external_secret import ExternalSecretSource, ExternalSecret
+from .external_aws_secrets_chart import ExternalAwsSecretsChart
---
+++ tests/eks/external_secrets/test_external_secrets_chart.py
@@ -0,0 +1,33 @@
+from cdk8s import App
+
+from ca_cdk_constructs.eks.external_secrets import (
+    ExternalSecretSource,
+)
+from ca_cdk_constructs.eks.external_secrets.external_aws_secrets_chart import (
+    ExternalAwsSecretsChart,
+)
+from ca_cdk_constructs.eks.imports.io.external_secrets import SecretStoreSpecProviderAwsService
+
+
+# to update the snapshot, run:
+#   poetry run pytest tests/cdk8s_lib/external_secrets/test_external_secrets_chart.py --snapshot-update
+def test_external_secret_chart(snapshot):
+    app = App()
+
+    source = ExternalSecretSource(
+        source_secret="db-secret",
+        secret_mappings={"username": "DB_USER"},
+        # deriving k8s_secret_name from source_secret
+        # k8s_secret_name="database-secret"
+    )
+
+    ExternalAwsSecretsChart(
+        app,
+        "ExternalSecretsDeployment",
+        region="eu-west-1",
+        service_account_name="SA",  # not in scope
+        secret_service=SecretStoreSpecProviderAwsService.SECRETS_MANAGER,
+        secret_sources=[source],
+        namespace="test-ns",
+    )
+    assert app.synth_yaml() == snapshot
