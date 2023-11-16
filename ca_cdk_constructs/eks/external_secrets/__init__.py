d00b355a5ff251ff507a6b54ad1ca97d277cab7e
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2023-11-16 12:24:08 +0000

    noqa init file for F401

--- ca_cdk_constructs/eks/external_secrets/__init__.py
+++ ca_cdk_constructs/eks/external_secrets/__init__.py
@@ -1,3 +1,4 @@
-from .external_secrets_aws_secret_store import ExternalSecretsAwsSecretStore  # noqa: F401
-from .external_secret import ExternalSecretSource, ExternalSecret  # noqa: F401
-from .external_aws_secrets_chart import ExternalAwsSecretsChart  # noqa: F401
+# ruff: noqa: F401
+from .external_secrets_aws_secret_store import ExternalSecretsAwsSecretStore
+from .external_secret import ExternalSecretSource, ExternalSecret
+from .external_aws_secrets_chart import ExternalAwsSecretsChart
