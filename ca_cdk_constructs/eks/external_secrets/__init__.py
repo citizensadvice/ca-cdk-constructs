384b801c548de75d883469ebce429495aa17741a
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-10-22 15:41:18 +0100

    Fix import error

--- ca_cdk_constructs/eks/external_secrets/__init__.py
+++ ca_cdk_constructs/eks/external_secrets/__init__.py
@@ -1,4 +1,6 @@
-# ruff: noqa: F401
-from .external_aws_secrets_chart import ExternalAwsSecretsChart
-from .external_secret import ExternalSecret, ExternalSecretSource
+# ruff: noqa: F401, I001
+# This will raise a circular import error unless the imports
+# are in this exact order, so isort needs to be ingnored.
 from .external_secrets_aws_secret_store import ExternalSecretsAwsSecretStore
+from .external_secret import ExternalSecretSource, ExternalSecret
+from .external_aws_secrets_chart import ExternalAwsSecretsChart
